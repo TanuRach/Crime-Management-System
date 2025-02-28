7f160689dc92b9ee0ed677ff1dab6d723dd995d0
Tanu <trachanaikar@gmail.com>
2025-03-01 00:14:13 +0530

    Login form creation

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,3 +1,10 @@
 export default function Login({ name }) {
-  return <p>Welcome {name}</p>;
+  return (
+    <>
+      <p>Welcome {name}</p>
+      <input type="text" aria-label="Username" />
+      <input type="text" aria-label="Password" />
+      <button>Login</button>
+    </>
+  );
 }
