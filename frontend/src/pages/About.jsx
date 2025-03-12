6a23be75afb19b24a0c677264e9090f4bde80f0e
Tanu <trachanaikar@gmail.com>
2025-03-12 16:18:27 +0530

    about page added

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,10 @@
+export default function about() {
+  return (
+    <h2>
+      <center>
+        About Page
+        <h3>About us? we don't know about us too</h3>
+      </center>
+    </h2>
+  );
+}
