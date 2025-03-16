29f3024fe69ce421f1d6d70c1cbae8f34f375d15
Tanu <trachanaikar@gmail.com>
2025-03-17 00:21:25 +0530

    About page added

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 export default function about() {
+  const navigate = useNavigate();
+
   return (
     <h2>
       <center>
         About Page
-        <h3>About us? we don't know about us too</h3>
+        <h3>CRIME MANAGEMENT SYSTEM TEAM</h3>
+        <button
+          onClick={() => navigate("/home")}
+          className=" px-6 py-3 bg-blue-500 text-white rounded-xl shadow-lg"
+        >
+          Back
+        </button>
       </center>
     </h2>
   );
