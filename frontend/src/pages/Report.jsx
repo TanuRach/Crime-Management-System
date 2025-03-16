a57d7a66352818b34f9b85d1fd19fe6df03c8a35
Tanu <trachanaikar@gmail.com>
2025-03-17 00:28:28 +0530

    Report page

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 export default function report() {
+  const navigate = useNavigate();
+
   return (
     <>
       <center>
-        <h4>want to report crime?</h4>
+        <h4>REPORT CRIME HERE</h4>
+        <button
+          onClick={() => navigate("/home")}
+          className=" px-6 py-3 bg-blue-500 text-white rounded-xl shadow-lg"
+        >
+          Back
+        </button>
       </center>
     </>
   );
