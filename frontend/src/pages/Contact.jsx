414d8447c91f2895824fdc42e2e3a31dd5d57814
Tanu <trachanaikar@gmail.com>
2025-03-17 00:22:34 +0530

    Contact page added

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 export default function contact() {
+  const navigate = useNavigate();
+
   return (
     <>
       <center>
         Contact Page
-        <h3>any problems please don't,hesitate to contact us</h3>
+        <h3>HELP-LINE NUMBER : 1234567</h3>
+        <button
+          onClick={() => navigate("/home")}
+          className=" px-6 py-3 bg-blue-500 text-white rounded-xl shadow-lg"
+        >
+          Back
+        </button>
       </center>
     </>
   );
