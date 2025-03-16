a617493bb6b13364960ad3a0c6005cd01bec0441
Tanu <trachanaikar@gmail.com>
2025-03-17 00:31:40 +0530

    App page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
+import Home from "./pages/Home";
+import Contact from "./pages/contact";
+import About from "./pages/about";
+import Report from "./pages/report";
+import NoPage from "./pages/NoPage";
+import Signup from "./pages/signup";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <Login name="Tanu" />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="flex justify-center items-center h-screen">
+              <Login name="Have an account? Login" />
+            </div>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/report" element={<Report />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NoPage />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
